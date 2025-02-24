947ba70e0b5c57a00fbb9f9157f9e6cec692e804
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2025-02-24 21:57:36 +0700

    update SQL schema

--- database.sql
+++ database.sql
@@ -5,8 +5,30 @@
 -- 3. How you name the fields.
 -- In this assignment we will use PostgreSQL as the database.
 -- This is test table. Remove this table and replace with your own tables. 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE
-	test (
-		id serial PRIMARY KEY,
-		name VARCHAR(50) UNIQUE NOT NULL
+	"estates" (
+		"id" uuid PRIMARY KEY DEFAULT (uuid_generate_v4 ()),
+		"length" integer NOT NULL,
+		"width" integer NOT NULL,
+		"created_at" timestamp NOT NULL DEFAULT (now ()),
+		"updated_at" timestamp NOT NULL DEFAULT (now ()),
+		"deleted_at" timestamp
 	);
+
+CREATE TABLE
+	"trees" (
+		"id" uuid PRIMARY KEY DEFAULT (uuid_generate_v4 ()),
+		"estate_id" uuid NOT NULL,
+		"x" integer NOT NULL,
+		"y" integer NOT NULL,
+		"height" integer NOT NULL,
+		"created_at" timestamp NOT NULL DEFAULT (now ()),
+		"updated_at" timestamp NOT NULL DEFAULT (now ()),
+		"deleted_at" timestamp
+	);
+
+CREATE UNIQUE INDEX ON "trees" ("estate_id", "x", "y");
+
+ALTER TABLE "trees" ADD FOREIGN KEY ("estate_id") REFERENCES "estates" ("id");
